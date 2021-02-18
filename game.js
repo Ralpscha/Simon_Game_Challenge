543774d683ddc1f1c12fccec1845083a85e16fd6
Ralph van Schaik <ralph.van.schaik@me.com>
2021-02-18 16:52:39 +0100

    Geluid werkt na druk op knop.

--- game.js
+++ game.js
@@ -1,23 +1,40 @@
-let gamePattern = [];
+$("#start").click(function() {
+  console.log("en we zijn los");
+  let gamePattern = [];
 
-let buttonColours = ["red", "blue", "green", "yellow"];
+  let buttonColours = ["red", "blue", "green", "yellow"];
 
-let randomChosenColour = buttonColours[nextSequence()];
+  let randomChosenColour = buttonColours[nextSequence()];
 // console.log(randomChosenColour);
-flashButton(randomChosenColour);
-gamePattern.push(randomChosenColour);
+  flashButton(randomChosenColour);
+  geluidAfspelen();
+
+  gamePattern.push(randomChosenColour);
 // console.log(gamePattern);
 
 
-function nextSequence() {
-  let randomNumber = Math.round(Math.random() * 3);
-  return randomNumber;
-}
+  function nextSequence() {
+    let randomNumber = Math.round(Math.random() * 3);
+    return randomNumber;
+  }
 
 
 
-function flashButton(randomChosenColour) {
-  // console.log(randomChosenColour);
-  $("#"+randomChosenColour).fadeOut(200).fadeIn(200);
-}
+  function flashButton(randomChosenColour) {
+    // console.log(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(200).fadeIn(200);
+  }
+
+  function geluidAfspelen() {
+    let buttonSound = new Audio();
+    buttonSound.src = "sounds/blue.mp3";
+    buttonSound.play();
+    console.log(buttonSound);
+  }
+
+});
+
+
+
+
 
