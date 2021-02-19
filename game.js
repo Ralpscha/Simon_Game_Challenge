3424e0cccc62ab5858b905aca8ecb8bd88e4b1b9
Ralph van Schaik <ralph.van.schaik@me.com>
2021-02-19 17:27:40 +0100

    nieuwe sequence als de oude net zo lang is

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ let userClickedPattern = [];
 let started = false;
 let level = 0;
 
+
 $(document).keypress(function (){
   if (!started) {
 
@@ -18,24 +19,28 @@ $(document).keypress(function (){
 
 $("button").click(function(){
   let userChosenColour = this.id;
+  playSound(userChosenColour);
   userClickedPattern.push(userChosenColour);
   animatePress(userChosenColour);
   console.log(userClickedPattern);
-  playSound(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
 
 
 });
 
 function nextSequence() {
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
   let randomNumber = Math.floor(Math.random() * 4);
   console.log(randomNumber);
   randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
 
-  $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+
+  $("#"+randomChosenColour).fadeOut(500).fadeIn(100);
   playSound(randomChosenColour);
+  gamePattern.push(randomChosenColour);
 }
 
 function playSound(geluid) {
@@ -51,7 +56,21 @@ function animatePress(currentColour) {
   },150);
 }
 
-// nextSequence();
+function checkAnswer(currentLevel) {
+  console.log("GamePattern: ", gamePattern);
+  console.log("UserPattern: ", userClickedPattern);
+
+  if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if(userClickedPattern.length === gamePattern.length) {
+      setTimeout(nextSequence, 2000);
+
+    }
+  } else {
+    console.log("wrong");
+  }
+
+}
 
 
 
