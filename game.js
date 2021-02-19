f262b00df0e7bbbe994be167f795a6d9e8abf060
Ralph van Schaik <ralph.van.schaik@me.com>
2021-02-19 11:21:24 +0100

    Opnieuw begonnen. Userclicks worden nu afgevangen.

--- game.js
+++ game.js
@@ -1,40 +1,29 @@
-$("#start").click(function() {
-  console.log("en we zijn los");
-  let gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+let randomChosenColour = "";
+let gamePattern = [];
+let userClickedPattern = [];
 
-  let buttonColours = ["red", "blue", "green", "yellow"];
-
-  let randomChosenColour = buttonColours[nextSequence()];
-// console.log(randomChosenColour);
-  flashButton(randomChosenColour);
-  geluidAfspelen();
-
-  gamePattern.push(randomChosenColour);
-// console.log(gamePattern);
-
-
-  function nextSequence() {
-    let randomNumber = Math.round(Math.random() * 3);
-    return randomNumber;
-  }
-
-
-
-  function flashButton(randomChosenColour) {
-    // console.log(randomChosenColour);
-    $("#"+randomChosenColour).fadeOut(200).fadeIn(200);
-  }
-
-  function geluidAfspelen() {
-    let buttonSound = new Audio();
-    buttonSound.src = "sounds/blue.mp3";
-    buttonSound.play();
-    console.log(buttonSound);
-  }
-
+$("button").click(function(){
+  let userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
 });
 
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  console.log(randomNumber);
+  randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
+  $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+  let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  console.log(audio);
+  // audio.play();
+}
+
+
+
+// nextSequence();
 
 
 
